44b93019efeefd0d53d8b21045c10444e9785913
jonny1811 <jonny@quanto.la>
2022-02-02 00:07:44 -0300

    fix: change the logic to scape the maze

--- day-6/main.py
+++ day-6/main.py
@@ -10,10 +10,10 @@ while front_is_clear():
 turn_left()
 
 while not at_goal():
-    if right_is_clear():
-        turn_right()
+    if front_is_clear() and wall_on_right():
         move()
     elif front_is_clear():
+        turn_right()
         move()
     else:
         turn_left()
