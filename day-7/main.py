5c94a8dd682d05261fba10ed020918e1b834d68d
jonny1811 <jonny@quanto.la>
2022-02-05 19:48:10 -0300

    add to replace underscore to correct letter in the word

--- day-7/main.py
+++ day-7/main.py
@@ -1,13 +1,19 @@
 import random
 word_list = ["ardvark", "baboon", "camel"]
 
-
 chosen_word = random.choice(word_list)
+
+print(f"Pssst, the solution is {chosen_word}")
+
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
 
 guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
+for position in range(word_length):
+    letter = chosen_word[position]
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = letter
+print(display)
