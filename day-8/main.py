5fbcc37b7b123841b19160cfd81ef37d03577e55
jonny1811 <jonny@quanto.la>
2022-02-17 22:25:51 -0300

    area calc cans paint

---
+++ day-8/main.py
@@ -0,0 +1,20 @@
+# def greet():
+#     print('Primer print 1')
+#     print('Primer print 2')
+#     print('Primer print 3')
+
+# greet()
+
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+    
+# greet_with_name("Jonny")
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+    
+# greet_with("Jonny", "Hernandarias")
+
+greet_with(location="Singapur", name="Jonny")
