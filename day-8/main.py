578c6d42d5016e482c70ed56403af10548239cd8
jonny1811 <jonny@quanto.la>
2022-02-25 17:26:49 -0300

    complete caesar cipher project

--- day-8/main.py
+++ day-8/main.py
@@ -1,20 +1,36 @@
-# def greet():
-#     print('Primer print 1')
-#     print('Primer print 2')
-#     print('Primer print 3')
+from art import logo
+print(logo)
 
-# greet()
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
+'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# def greet_with_name(name):
-#     print(f"Hello {name}")
-#     print(f"How do you do {name}?")
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char    
+        
+    print(f"The {cipher_direction}d text is {end_text}")
+
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
     
-# greet_with_name("Jonny")
-
-def greet_with(name, location):
-    print(f"Hello {name}")
-    print(f"What is it like in {location}")
-    
-# greet_with("Jonny", "Hernandarias")
-
-greet_with(location="Singapur", name="Jonny")
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if result == "no":
+        should_continue = False
+        print("Goodbye")
